77495c833eef7b48aa3ed974c2365386696befc5
Joan173 <jonpublic@hotmail.es>
2016-12-22 00:56:07 +0100

    Add files via upload
    Versión esqueleto del juego Lunar Landing que incluye:
    
    * Html con los elementos básicos del juego
    * Css: d.css y m.css dos versiones que cargan mediante media query dependiendo del tamaño de pantalla (pruébalo!)
    * Js: con el javascript necesario para dejar caer la nave y parar cuando llega a un límite. Actualiza la velocidad y la altura en %/s y % (1% de pantalla = 1 metro).

---
+++ js.js
@@ -0,0 +1,117 @@
+var y = 10; // altura inicial y0=10%, debe leerse al iniciar si queremos que tenga alturas diferentes dependiendo del dispositivo
+var v = 0;
+var g = 1.622;
+var a = g;
+var dt = 0.016683;
+var timer=null;
+var timerFuel=null;
+var fuel=100;
+var umbral_v = 5;
+var fin=false;
+
+//al cargar por completo la página...
+window.onload = function(){
+	//definición de eventos
+	//mostrar menú móvil
+    document.getElementById("showm").onclick = function () {
+		document.getElementsByClassName("c")[0].style.display = "block";
+		stop();
+	}
+	//ocultar menú móvil
+	document.getElementById("hidem").onclick = function () {
+		document.getElementsByClassName("c")[0].style.display = "none";
+		start();
+	}
+	
+	document.getElementById("menu_jugar").onclick = function (el) {
+		y = 10;
+		v = 0;
+		fuel = 100;
+		document.getElementById("img_nave").src = "img/nave_on.png";
+		
+		fin = false;
+		stop();
+		start();
+	}
+	
+	//encender/apagar el motor al hacer click en la pantalla
+	document.onclick = function (el) {
+
+			// si el juego ha terminado no hacer nada
+			if (fin) { return; }
+
+			if (a==g){
+			motorOn();
+			} else {
+			motorOff();
+			}
+	}
+	//encender/apagar al apretar/soltar una tecla
+	document.onkeydown = motorOn;
+	document.onkeyup = motorOff;
+	
+	//Empezar a mover nave
+	start();
+}
+
+//Definición de funciones
+function start(){
+	timer=setInterval(function(){ moverNave(); }, dt*1000);
+}
+
+function stop(){
+	clearInterval(timer);
+	clearInterval(timerFuel);
+}
+
+function moverNave(){
+	v +=a*dt;
+	document.getElementById("velocidad").innerHTML=v;
+	y +=v*dt;
+	document.getElementById("altura").innerHTML=y;
+	
+	//mover hasta que top sea un 70% de la pantalla
+	if (y<70){ 
+		document.getElementById("nave").style.top = y+"%"; 
+	} else { 
+		if (v>=umbral_v) {
+			document.getElementById("img_nave").src = "img/nave_rota.png";
+		}
+		else {
+			document.getElementById("img_nave").src = "img/nave.png";
+		}
+		fin = true;
+		stop();
+	}
+}
+function motorOn(){
+	// si el juego ha terminado no hacer nada
+	if (fin==true) { return; }
+		
+	document.getElementById("img_nave").src = "img/nave_on.png";
+	
+	document.getElementById("msg").innerHTML="ON";
+	
+	a=-g;
+	if (timerFuel==null) {
+		timerFuel=setInterval(function(){ actualizarFuel(); }, 10);
+	}
+}
+
+function motorOff(){
+	// si el juego ha terminado no hacer nada
+	if (fin==true) { return; }
+
+	document.getElementById("img_nave").src = "img/nave.png";
+	
+	document.getElementById("msg").innerHTML="OFF";
+	
+	a=g;
+	clearInterval(timerFuel);
+	timerFuel=null;
+}
+function actualizarFuel(){
+	//Aquí hay que cambiar el valor del marcador de Fuel...
+	fuel-=0.1;
+	document.getElementById("fuel").innerHTML=fuel;	
+}
